cc4cca5eb56e4ff4767a6200f12c1e7fea8d65ee
rhartant@uci.edu <44073902+rhartant@users.noreply.github.com>
2019-10-25 17:49:47 -0700

    Ryan scene. Player sprite with movement.

---
+++ Assets/Scripts/PlayerCopterMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCopterMovement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Handles copter movement (kind of drifty, like a helicopter)
+        if (Input.GetButton("Up"))
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * moveSpeed);
+        if (Input.GetButton("Down"))
+            GetComponent<Rigidbody2D>().AddForce(Vector2.down * moveSpeed);
+        if (Input.GetButton("Left"))
+            GetComponent<Rigidbody2D>().AddForce(Vector2.left * moveSpeed);
+        if (Input.GetButton("Right"))
+            GetComponent<Rigidbody2D>().AddForce(Vector2.right * moveSpeed);
+    }
+}
---
+++ Assets/Scripts/PlayerDashMechanic.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDashMechanic : MonoBehaviour
+{
+    private Camera m_MainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_MainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mouseDirection = m_MainCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+        if (Input.GetKeyDown("left shift"))
+        {
+            GetComponent<Rigidbody2D>().AddForce(mouseDirection * 150);
+        }
+    }
+}
