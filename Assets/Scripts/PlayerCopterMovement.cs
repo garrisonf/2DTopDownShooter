32b5f5c1f9c644ac141e8a57b9400554c852a06e
Garrison Finley <garrisonfinley@gmail.com>
2019-12-10 00:20:07 -0800

    Add new copter movement

--- Assets/Scripts/PlayerCopterMovement.cs
+++ Assets/Scripts/PlayerCopterMovement.cs
@@ -5,23 +5,33 @@ using UnityEngine;
 public class PlayerCopterMovement : MonoBehaviour
 {
     public float moveSpeed = 10f;
+    public float maxSpeed = 30f;
+    public float opposingForceMultiplier = 2;
+
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         // Handles copter movement (kind of drifty, like a helicopter)
-        if (Input.GetButton("Up"))
-            GetComponent<Rigidbody2D>().AddForce(Vector2.up * moveSpeed);
-        if (Input.GetButton("Down"))
-            GetComponent<Rigidbody2D>().AddForce(Vector2.down * moveSpeed);
-        if (Input.GetButton("Left"))
-            GetComponent<Rigidbody2D>().AddForce(Vector2.left * moveSpeed);
-        if (Input.GetButton("Right"))
-            GetComponent<Rigidbody2D>().AddForce(Vector2.right * moveSpeed);
+        Vector2 inputDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        var oppositeDirAmount = Mathf.Clamp01(Dot(rb.velocity.normalized, -inputDirection));
+        var effectiveSpeed = Mathf.Lerp(moveSpeed, moveSpeed * opposingForceMultiplier, oppositeDirAmount);
+        rb.AddForce(inputDirection * effectiveSpeed * Time.deltaTime, ForceMode2D.Impulse);
+    }
+
+    void FixedUpdate(){
+        if(rb.velocity.magnitude > maxSpeed){
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+    }
+
+    private float Dot(Vector3 first, Vector3 second){
+        return first.x * second.x + first.y * second.y + first.z * second.z;
     }
 }
