a7936de48a18ec9a95739fbee76dc40ab0404e88
Jéssica Ione <jessicaione101@gmail.com>
2020-01-30 12:09:04 -0800

    Add some preliminary scripts

--- Assets/Scripts/LightPuzzleScripts/LightPuzzle.cs
+++ Assets/Scripts/LightPuzzleScripts/LightPuzzle.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class LightPuzzle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public int width;
+  public int height;
+  public BoxTile[,] boxes;
+  
+  void Start()
+  {
+    boxes = new BoxTile[height, width];
+    Transform box_group = this.gameObject.transform.Find("Boxes");
+    
+    for (int i = 0; i < box_group.childCount; ++i)
+      print(box_group.GetChild(i).GetComponent<BoxTile>().box_name);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    box_group.GetChild(8).GetComponent<BoxTile>().box_name = "LOOOOOOOL";
+    
+    box_group = this.gameObject.transform.Find("Boxes");
+    for (int i = 0; i < box_group.childCount; ++i)
+      print(box_group.GetChild(i).GetComponent<BoxTile>().box_name);
+    
+    
+        //boxes = GetComponentsInChildren<BoxTile>();
+
+        //foreach (BoxTile b in boxes)
+            //print(b.box_name);
+  }
 }
