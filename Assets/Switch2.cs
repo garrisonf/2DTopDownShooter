f5e64876a6343847d205259c0d427b879f992329
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2020-02-20 12:10:49 -0800

    Add assets

---
+++ Assets/Switch2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Switch2 : MonoBehaviour
+{
+    //Start is called before the first frame update
+    Color originalColor = Color.white;
+    void Start()
+    {
+        GetComponent<SpriteRenderer>().color = originalColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the box collider is overlapping with the player's collider
+        if(GetComponent<Collider2D>().IsTouching(GameObject.FindWithTag("Player").GetComponent<Collider2D>()))
+        {
+            // If the user presses space
+            if(Input.GetKeyDown("space"))
+            {
+               StartCoroutine(waitForSwitch());
+               //GameObject.FindWithTag("VerticalBar").GetComponent<Slider>().value += 0.2f;
+               GameObject.FindWithTag("VerticalBar").GetComponent<VerticalBar>().IncrementProgress(0.1f);
+            }
+        }
+    }
+    
+    IEnumerator waitForSwitch()
+    {
+       GetComponent<SpriteRenderer>().color = Color.green;
+       yield return new WaitForSeconds(0.2f);
+       GetComponent<SpriteRenderer>().color = originalColor;
+    }
+}
