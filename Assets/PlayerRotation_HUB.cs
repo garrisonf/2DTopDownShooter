d32d10e1ec89c37f815b4f122b84e658fa421c1d
Ahlon Sin <56621535+ahls@users.noreply.github.com>
2020-03-03 21:04:35 -0800

    Changed a lot in the Sin Hubworld and the models

---
+++ Assets/PlayerRotation_HUB.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation_HUB : MonoBehaviour
+{
+    private Camera mainCam;
+    public bool instant = false;
+    public float speed = 7;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray cameraRay = mainCam.ScreenPointToRay(Input.mousePosition);
+        Plane screenFloor = new Plane(Vector3.forward, Vector3.zero);
+        float rayLength;
+        screenFloor.Raycast(cameraRay, out rayLength);
+        Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+        Vector2 mouseDirection = pointToLook-transform.position;
+        float mouseAngle = Mathf.Atan2(mouseDirection.y, mouseDirection.x) * Mathf.Rad2Deg;
+        Quaternion desiredRotation = Quaternion.AngleAxis(mouseAngle, Vector3.forward);
+        if (instant)
+        {
+            transform.rotation = desiredRotation;
+        }
+        else
+        {
+            transform.rotation = Quaternion.RotateTowards(
+                transform.rotation,
+                desiredRotation,
+                speed * Time.deltaTime * Mathf.Rad2Deg
+            );
+        }
+    }
+}
--- Assets/Scripts/PlayerRotation.cs
+++ Assets/Scripts/PlayerRotation.cs
@@ -21,11 +21,11 @@ public class PlayerRotation : MonoBehaviour
 
 	private void Update()
 	{
+        
 		Vector2 mouseDirection = m_MainCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
 		float mouseAngle = Mathf.Atan2(mouseDirection.y, mouseDirection.x) * Mathf.Rad2Deg;
 		Quaternion desiredRotation = Quaternion.AngleAxis(mouseAngle, Vector3.forward);
-
-		if (instant)
+        if (instant)
 		{
 			transform.rotation = desiredRotation;
 		}
